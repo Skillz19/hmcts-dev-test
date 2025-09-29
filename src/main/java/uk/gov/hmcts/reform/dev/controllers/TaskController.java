b55268da340565ab264c03cdac1c96b859bc1e24
Skillz19 <54452306+Skillz19@users.noreply.github.com>
2025-09-29 23:38:56 +0100

    feat: add task controller endpoint to retrieve task by id

---
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.dev.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import uk.gov.hmcts.reform.dev.models.Task;
+import uk.gov.hmcts.reform.dev.services.TaskService;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id)
+                .map(task -> ResponseEntity.ok(task))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+}
