4176e972b65ea2ef3db12e6f538ddffe893a793b
Skillz19 <skillz@SKILLZ-PC.>
2025-09-30 23:19:02 +0100

    feat: add custom filter for njk templates

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -10,10 +10,21 @@ export class Nunjucks {
 
   enableFor(app: express.Express): void {
     app.set('view engine', 'njk');
-    nunjucks.configure(path.join(__dirname, '..', '..', 'views'), {
+    const env = nunjucks.configure(path.join(__dirname, '..', '..', 'views'), {
       autoescape: true,
       watch: this.developmentMode,
       express: app,
+    });
+
+    // Add a custom date filter
+    env.addFilter('formatDate', (date: string | Date) => {
+      return new Date(date).toLocaleString('en-GB', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      });
     });
 
     app.use((req, res, next) => {
