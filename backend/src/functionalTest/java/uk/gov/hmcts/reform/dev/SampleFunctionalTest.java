3a0671875e7215168f17b6143b5438d8010f5014
Skillz19 <skillz@SKILLZ-PC.>
2026-02-09 21:49:23 +0000

    test: make functional and smoke tests self-contained with random port

--- backend/src/functionalTest/java/uk/gov/hmcts/reform/dev/SampleFunctionalTest.java
+++ backend/src/functionalTest/java/uk/gov/hmcts/reform/dev/SampleFunctionalTest.java
@@ -6,32 +6,32 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SampleFunctionalTest {
-    protected static final String CONTENT_TYPE_VALUE = "application/json";
 
-    @Value("${TEST_URL:http://localhost:8080}")
-    private String testUrl;
+    @LocalServerPort
+    private int port;
 
     @BeforeEach
     public void setUp() {
-        RestAssured.baseURI = testUrl;
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
         RestAssured.useRelaxedHTTPSValidation();
     }
 
     @Test
     void functionalTest() {
         Response response = given()
-            .contentType(ContentType.JSON)
-            .when()
-            .get()
-            .then()
-            .extract().response();
+                .contentType(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .extract().response();
 
         Assertions.assertEquals(200, response.statusCode());
         Assertions.assertTrue(response.asString().startsWith("Welcome"));
